00b77fa015bf96a9b851044215bd8df4b7c491bb
MaM00d <mahmoudessamfathy@gmail.com>
2024-03-02 16:43:53 +0200

    auth folder

--- user/api.go
+++ user/api.go
@@ -6,9 +6,6 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
-	"os"
-
-	jwt "github.com/golang-jwt/jwt/v4"
 
 	api "Server/elapi"
 )
@@ -97,43 +94,6 @@ func (s *ElUser) handleGetUserByEmailFromVars(w http.ResponseWriter, r *http.Req
 	return fmt.Errorf("method not allowed %s", r.Method)
 }
 
-func (eluser *ElUser) JWTAuthMiddleware(w http.ResponseWriter, r *http.Request) error {
-	fmt.Println("calling JWT auth middleware")
-	tokenString := r.Header.Get("x-jwt-token")
-
-	token, err := detokenizejwt(tokenString)
-	if err != nil {
-		eluser.ap.PermissionDenied(w)
-	}
-	if !token.Valid {
-
-		fmt.Println("err 2")
-		eluser.ap.PermissionDenied(w)
-	}
-	userEmail := eluser.getEmailFromVars(r)
-
-	user, err := eluser.store.GetUserByEmail(userEmail)
-	if err != nil {
-
-		fmt.Println("err 3")
-		eluser.ap.PermissionDenied(w)
-		eluser.ap.PermissionDenied(w)
-	}
-
-	claims := token.Claims.(jwt.MapClaims)
-
-	if user.Email != claims["userEmail"] {
-
-		fmt.Println("err 4")
-		eluser.ap.PermissionDenied(w)
-	}
-
-	if err != nil {
-		eluser.ap.WriteJSON(w, http.StatusForbidden, api.ApiError{Error: "invalid token"})
-	}
-	return eluser.handleGetUserByEmailFromVars(w, r)
-}
-
 func (s *ElUser) handleGetUser(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
@@ -142,33 +102,7 @@ func (s *ElUser) handleDeleteUser(w http.ResponseWriter, r *http.Request) error 
 	return nil
 }
 
-func tokenizejwt(eluser *User) (string, error) {
-	claims := &jwt.MapClaims{
-		"expiresAt": 15000,
-		"userEmail": eluser.Email,
-	}
-
-	secret := os.Getenv("JWT_SECRET")
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	return token.SignedString([]byte(secret))
-}
-
 func (eluser *ElUser) getEmailFromVars(r *http.Request) string {
 	elemail := eluser.ap.GetFromVars(r, "email")
 	return elemail
-}
-
-func detokenizejwt(tokenString string) (*jwt.Token, error) {
-	secret := os.Getenv("JWT_SECRET")
-
-	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
-
-		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
-		return []byte(secret), nil
-	})
 }
